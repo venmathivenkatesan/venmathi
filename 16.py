8d0fc6554ee587196cda8bd3f57246f50e800047
venmathivenkatesan <52342535+venmathivenkatesan@users.noreply.github.com>
2019-07-01 15:35:22 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -1,7 +1,7 @@
 vv,vad=map(int,input().split())
-for m in range(vv+1,vad,1)
+for m in range(vv+1,vad,1):
     if(m>1):
-        for n in range(2,n):
+        for n in range(2,m):
             if(m%n==0):
                 break
         else:
