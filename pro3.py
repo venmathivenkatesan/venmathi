4e37597734107bb7256e6521f481a77cf2044981
venmathivenkatesan <52342535+venmathivenkatesan@users.noreply.github.com>
2019-07-09 17:47:55 +0530

    Update pro3.py

--- pro3.py
+++ pro3.py
@@ -1,6 +1,6 @@
 v,m=input().split()
-jk=nila(len(m)-len(v))
-for i in range(len(v)):
+jk=abs(len(m)-len(v))
+for g in range(len(v)):
     if(len(m)==1 and m[g] in v):
         break
     if (v[g]!=m[g]):
