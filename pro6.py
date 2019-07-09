61fa205dae6cfda46bdd2aad3224f13a269513a7
venmathivenkatesan <52342535+venmathivenkatesan@users.noreply.github.com>
2019-07-09 18:15:00 +0530

    Update pro6.py

--- pro6.py
+++ pro6.py
@@ -3,7 +3,7 @@ array=list(map(int,input().split()))
 vm=0
 for h in range(len(array)-2):
     for z in range(h+1,len(array)-1):
-        for o in range(z+1,len(array)):
-            if array[h]<array[z]<array[o] and h<z<o:
-                vm=vm+o
+        for p in range(z+1,len(array)):
+            if array[h]<array[z]<array[p] and h<z<p:
+                vm=vm+p
 print(vm)
