6cceec4037025570c47f444bd25e2bb2dad5ccaf
venmathivenkatesan <52342535+venmathivenkatesan@users.noreply.github.com>
2019-07-12 16:54:18 +0530

    Create pl_9.py

---
+++ pl_9.py
@@ -0,0 +1,11 @@
+#a    
+v,k=list(map(int,input().split()))
+vk=0
+for j in range(v,k+1):
+    if j>1:
+        for g in range(2,g):
+            if(g%x==0):
+                break
+        else:
+            vk+=1
+print(vk)
