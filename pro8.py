600244cf2dd5f45ac019d234c7065cf538512c29
venmathivenkatesan <52342535+venmathivenkatesan@users.noreply.github.com>
2019-07-27 10:54:13 +0530

    Update pro8.py

--- pro8.py
+++ pro8.py
@@ -1,12 +1,11 @@
-#GCD
 import math
-q=[]
-v,m=map(int,input().split())
-q1=list(map(int,input().split()))
-for x in range(m):
-    e,r=map(int,input().split())
-    q.append([e,r])
-for y in q:
-     c1=y[0]-1
-     c2=y[1]-1
-     print(math.gcd(q1[c1],q1[c2]))
+v,n=map(int,input().split())
+g=[]
+bbb=list(map(int,input().split()))
+for k in range(0,n):
+    vn,asf=map(int,input().split())
+    g.append([vn,asf])
+for k in g:
+    ven=k[0]-1
+    thi=k[1]-1
+    print(math.gcd(bbb[ven],bbb[thi]))
