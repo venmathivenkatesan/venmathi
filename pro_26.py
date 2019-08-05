d08bd356cca1afea5f14256a4526383514f36753
venmathivenkatesan <52342535+venmathivenkatesan@users.noreply.github.com>
2019-08-05 14:58:16 +0530

    Create pro_26.py

---
+++ pro_26.py
@@ -0,0 +1,14 @@
+def sub(m): 
+    ven = len(m) 
+    subb = [1]*ven
+    for i in range (1 , ven): 
+        for j in range(0 , i): 
+            if m[i] > m[j] and subb[i]< subb[j] + 1 : 
+                subb[i] = subb[j]+1
+    maximum = 0
+    for i in range(ven): 
+        maximum = max(maximum , subb[i])  
+    return maximum
+ar1=int(input()) 
+m = list(map(int,input().split()))
+print (subb(m))
